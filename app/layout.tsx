38104993286d5078d5b686714f5fe9a9babe0e14
Piyush Goel <34750712+piyushgoell@users.noreply.github.com>
2024-09-08 11:58:59 +0530

    Updated Configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
-import type { Metadata } from 'next'
+// import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Piyush Goel - Senior Integration Engineer',
-  description: 'Portfolio of Piyush Goel, Senior Integration Engineer and YouTube Educator',
-}
 
 export default function RootLayout({
   children,
